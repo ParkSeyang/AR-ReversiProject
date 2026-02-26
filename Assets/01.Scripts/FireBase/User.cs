882e17a9b5d2190a642ab99c5ae7ed7836910e54
ParkSeyang <ghlwkd14@naver.com>
2026-02-26 13:34:01 +0900

    2026.02.26 로그인관련 수업
    파이어베이스로 로그인, 아이디생성, 로그아웃 관련 내용

---
+++ Assets/01.Scripts/FireBase/User.cs
@@ -0,0 +1,11 @@
+﻿
+// User의 DataModel이다 라고 부릅니다.
+public class User
+{
+    public string ID { get; set; }
+    public string Email { get; set; }
+    public string DisplayName { get; set; }
+
+    public static User Empty = new User();
+
+}
